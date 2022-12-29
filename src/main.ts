10e232e2a519a57879093b24e03dd85571d22cbe
Geoffrey Olson <geoffrey.olson@gmail.com>
2022-12-28 22:47:18 -0800

    move import statement to top of file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "./style.scss";
+
 const root = document.querySelector<HTMLElement>(":root");
 const date = new Date();
 const hours = (date.getHours() % 12) * 60; // the hour in minutes for a 12 hour clock
@@ -6,5 +8,3 @@ const seconds = date.getSeconds();
 root?.style.setProperty("--hours", (hours + minutes) * 0.5 + "deg");
 root?.style.setProperty("--minutes", (minutes * 60 + seconds) * 0.1 + "deg");
 root?.style.setProperty("--seconds", seconds * 6 + "deg");
-
-import "./style.scss";
